3d2f4d709da50ff67416ee686966cbfc60d5889c
Marchin Ohashi <mohashi@20L0209POS.local>
2023-04-14 13:02:00 -0700

    Simplify the script

--- post_notion.rb
+++ post_notion.rb
@@ -2,50 +2,49 @@ require "net/http"
 require "uri"
 require "json"
 
-NOTION_TOKEN = ENV["TOKEN"]
-DATABASE_ID = ENV["DATABASE_ID"]
+NOTION_TOKEN=ENV["TOKEN"]
+DATABASE_ID=ENV["DATABASE_ID"]
 
-def post_notion(title, category)
-  if category
-    cat = "仕事"
-  else
-    cat = "プライベート"
-  end
+def post_notion(title)
   uri = URI.parse("https://api.notion.com/v1/pages")
   request = Net::HTTP::Post.new(uri)
   request.content_type = "application/json"
   request["Authorization"] = "Bearer #{NOTION_TOKEN}"
   request["Notion-Version"] = "2022-06-28"
-  request.body =
-    JSON.dump(
-      {
-        "parent" => {
-          "database_id" => "#{DATABASE_ID}"
-        },
-        "properties" => {
-          # Replace Name with your title name
-          "Name" => {
-            "title" => [{ "text" => { "content" => "#{title}" } }]
-          },
-          # Comment out or replace this property with your prefered property
-          "Category" => {
-            "select" => {
-              "name" => "#{cat}"
+  request.body = JSON.dump({
+    "parent" => {
+      "database_id" => "#{DATABASE_ID}"
+    },
+    "properties" => {
+      # Replace Name with your title name
+      "Name" => {
+        "title" => [
+          {
+            "text" => {
+              "content" => "#{title}"
             }
           }
+        ]
+      },
+      # Comment out or replace this property with your prefered property
+      "Category" => {
+        "select" => {
+          "name" => "プライベート"
         }
       }
-    )
+    }
+  })
 
-  response =
-    Net::HTTP.start(uri.hostname, uri.port, use_ssl: true) do |http|
-      http.request(request)
-    end
+  response = Net::HTTP.start(uri.hostname, uri.port, use_ssl: true) do |http|
+    http.request(request)
+  end
 
   code = response.code
 
-  # Print error message if the response code is not 200 for debug purpose
-  STDERR.print "Error!\n#{response.body}\n" unless code == "200"
+  # Print error message if the response code is not 200
+  unless code == "200"
+    STDERR.print "Error!\n#{response.body}\n"
+  end
 end
 
-post_notion(ARGV[0], ARGV[1])
+post_notion(ARGV[0])
